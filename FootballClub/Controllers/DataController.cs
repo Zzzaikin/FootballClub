b36f95d5e8777afe30e84fb857dfd9be3e86791e
Заикин Дмитрий Владимирович <zaikin.pakedovo@gmail.com>
2021-11-08 16:15:00 +0300

    Изменил DataController (тест ветвления).

--- FootballClub/Controllers/DataController.cs
+++ FootballClub/Controllers/DataController.cs
@@ -11,29 +11,11 @@ namespace FootballClub.Controllers
     [Route("[controller]")]
     public class DataController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<DataController> _logger;
 
         public DataController(ILogger<DataController> logger)
         {
             _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
         }
     }
 }
