8eb73476bcb047d94fca03bc35f7849ab7847899
Заикин Дмитрий Владимирович <zaikin.pakedovo@gmail.com>
2021-11-08 15:28:17 +0300

    Переименовал контроллер.

--- FootballClub/Controllers/DataController.cs
+++ FootballClub/Controllers/DataController.cs
@@ -9,16 +9,16 @@ namespace FootballClub.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class DataController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<DataController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public DataController(ILogger<DataController> logger)
         {
             _logger = logger;
         }
